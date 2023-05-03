b76dfa41faa106ba2c7ab454ac906bb3f3eeaffa
NhatTruong <truongtnse130426@fpt.edu.vn>
2023-05-03 11:31:30 +0700

    first commit

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderApplicationServiceImpl.java
@@ -0,0 +1,33 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.ports.input.service.OrderApplicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Slf4j
+@Validated
+@Service
+class OrderApplicationServiceImpl implements OrderApplicationService {
+
+    private final OrderCreateCommandHandler orderCreateCommandHandler;
+    private final OrderTrackCommandHandler orderTrackCommandHandler;
+
+    OrderApplicationServiceImpl(OrderCreateCommandHandler orderCreateCommandHandler, OrderTrackCommandHandler orderTrackCommandHandler) {
+        this.orderCreateCommandHandler = orderCreateCommandHandler;
+        this.orderTrackCommandHandler = orderTrackCommandHandler;
+    }
+
+    @Override
+    public CreateOrderCommand createOrder(CreateOrderCommand createOrderCommand) {
+        return null;
+    }
+
+    @Override
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+        return null;
+    }
+}
